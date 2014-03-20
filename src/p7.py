c626a7fa3296ef5c3b16b5859f03b6f350e68d26
Carolina Yanes <alu0100533224@ull.edu.es>
2014-03-20 12:42:08 +0000

    first commit

--- src/p7.py
+++ src/p7.py
@@ -34,3 +34,14 @@ for i in range (len(tupla)):
   print matriz[i][0], #
   for j in range (1,4):
     print "\t%1.10f" % matriz[i][j], #
+    
+    
+    
+    
+    
+    
+    
+#ejercicios
+#1. creamos el archivo modulo que realiza lo que se pide (practica 6)y desde el archivo p7 llamamos al codigo y utilizamos el codigo 2.
+#2 el numero maximo de elementos de la t-upla depende de la memoria de cada maquina. se producen errires oara los elementos demasiado grandes, lo que habra que escribirlos en notacion cientifica. la extension pyc podra ser reconocida dependiendo de la utilizacion del if __name__="__main__"
+#3 hay que añadir la funcion time al for para  que diga el tiempo que tarda cada iteracion
